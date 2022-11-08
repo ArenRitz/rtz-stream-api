ec59a045c4e0a83b20ffe38ba346ff33077896eb
ArenRitz <aren.dolbakian@gmail.com>
2022-11-08 18:41:42 -0500

    revert

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,24 @@
 // routes/users.js
 const express = require('express');
 const router = express.Router();
+const users = require('../db/queries/users');
 require('dotenv').config();
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, process.env.DIR);
-  },
-  filename: function (req, file, cb) {
-    cb(null, uuidv4() + '.mp4')
-  },
-})
-const upload = multer({ storage: storage })
+var fs = require('fs');
 
 
-module.exports = (db) => {
-  // all routes will go here 
-  router.post('/upload', upload.single('rtzvid'), (req, res) => {
-    console.log(`Video uploaded: ${req.file.filename.substring(0, req.file.filename.lastIndexOf('.')) || req.file.filename}`)
-    // make post request to db
-    
-    
-  });
+/* GET users listing. */
+router.get('/:id', (req, res) => {
+  const path = process.env.DIR + req.params.id;
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const head = {
+    'Content-Length': fileSize,
+    'Content-Type': 'video/mp4',
+  }
+  res.writeHead(200, head)
+  fs.createReadStream(path).pipe(res)
+  
 
-  return router;
-}
+});
+
+module.exports = router;
