13ca25b4f103ac1b5331b1c2075ce022f147f161
ArenRitz <aren.dolbakian@gmail.com>
2022-11-08 17:13:42 -0500

    Update filename structure

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-      cb(null, process.env.DIR);
+    cb(null, process.env.DIR);
   },
   filename: function (req, file, cb) {
-      cb(null,  uuidv4() + '.mp4'  )
+    cb(null, uuidv4() + '.mp4')
   },
 })
 const upload = multer({ storage: storage })
@@ -16,14 +16,28 @@ const upload = multer({ storage: storage })
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
 router.post('/upload', upload.single('rtzvid'), (req, res) => {
-  console.log(`Video uploaded: ${req.file.filename}`)
-  })
+  console.log(`Video uploaded: ${req.file.filename.substring(0, filename.lastIndexOf('.')) || req.file.filename}`)
+  
+});
 
+router.get('/:id', (req, res) => {
+  const path = process.env.DIR + '/' + req.params.id + '.mp4';
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const head = {
+    'Content-Length': fileSize,
+    'Content-Type': 'video/mp4',
+  }
+  res.writeHead(200, head)
+  fs.createReadStream(path).pipe(res)
+
+
+});
 
 module.exports = router;
