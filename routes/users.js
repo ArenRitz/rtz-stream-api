f07508a8b70c6a454384566dd9e5662f3d467098
ArenRitz <aren.dolbakian@gmail.com>
2022-11-08 18:31:46 -0500

    DB setup

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,18 @@
 // routes/users.js
 const express = require('express');
 const router = express.Router();
-const users = require('../db/queries/users');
 require('dotenv').config();
-var fs = require('fs');
 
 
-/* GET users listing. */
-router.get('/:id', (req, res) => {
-  const path = process.env.DIR + '/' + req.params.id + '.mp4';
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
-  }
-  res.writeHead(200, head)
-  fs.createReadStream(path).pipe(res)
-  
 
-});
+module.exports = (db) => {
+  // all routes will go here 
+  router.post('/upload', upload.single('rtzvid'), (req, res) => {
+    console.log(`Video uploaded: ${req.file.filename.substring(0, req.file.filename.lastIndexOf('.')) || req.file.filename}`)
+    // make post request to db
+    
+    
+  });
 
-module.exports = router;
+  return router;
+}
